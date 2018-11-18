ae0657aed94546c88f79329148e91ab02377b4b5
Darin Manley <darin.j.manley@gmail.com>
2018-11-18 12:05:52 -0500

    update content
    update content

--- constants/ApiKeys.js
+++ constants/ApiKeys.js
@@ -1,10 +1,10 @@
 export default {
     FirebaseConfig: {
-        apiKey: "AIzaSyD1I6vNEaJLD0y8wFJtplboyKMUjlkhRjE",
-        authDomain: "bugleapp-4b94d.firebaseapp.com",
-        databaseURL: "https://bugleapp-4b94d.firebaseio.com/",
-        projectId: "bugleapp-4b94d",
-        storageBucket: "gs://bugleapp-4b94d.appspot.com",
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
         messagingSenderId: ""
     }
 }
